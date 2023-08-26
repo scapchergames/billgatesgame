d9b09531054441c21446f8f6bf23e8eafbe52f78
Melih Bahri AKTAN <redgame34@gmail.com>
2023-08-26 17:10:48 +0300

    name changes

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,8 +4,8 @@ import React from 'react';
 function Navbar() {
     return (
         <Box alignItems='center' mb={5}>
-            <Image src='https://neal.fun/spend/billgates.jpg' m='auto' borderRadius='full' />
-            <Text fontSize={40} mt={2} mb={2} fontWeight={700}>Spend Bill Gates' Money</Text>
+            <Image src='/elon-musk-logo.png' m='auto' borderRadius='full' />
+            <Text fontSize={40} mt={2} mb={2} fontWeight={700}>Spend Elon Musk' Money</Text>
         </Box>
        
     );
